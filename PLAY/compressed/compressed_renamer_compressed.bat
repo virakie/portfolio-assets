c6a301a66ee7d6dafb3673b11858f7b41d4f6da0
virakie <justvirak@email.com>
2026-04-09 14:59:54 +0700

    Add video assets

--- PLAY/compressed/compressed_renamer_compressed.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for %%f in (*.*) do (
-    set "fname=%%~nf"
-    set "ext=%%~xf"
-    setlocal enabledelayedexpansion
-    set "newname=!fname:compressO-=!"
-    ren "%%f" "!newname!_compressed!ext!"
-    endlocal
-)
